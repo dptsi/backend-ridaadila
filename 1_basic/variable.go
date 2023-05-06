970c9d1856d418e9bca10da7d7f8c3bf46095b11
ridaadila <ridaadila10@gmail.com>
2023-05-06 21:44:25 +0700

    add variable

---
+++ 1_basic/variable.go
@@ -0,0 +1,35 @@
+package main
+
+import "fmt"
+
+func main() {
+	var nama string
+
+	nama = "Rida Adila"
+
+	fmt.Println(nama)
+
+	nama = "Test 123"
+
+	fmt.Println(nama)
+
+	var sum int8 = 100
+
+	fmt.Println(sum)
+
+	alamat := "surabaya"
+
+	fmt.Println(alamat)
+
+	alamat = "Sidoarjo"
+
+	fmt.Println(alamat)
+
+	var (
+		firstName string = "Rida"
+		angka     int    = 100
+	)
+
+	fmt.Println(firstName)
+	fmt.Println(angka)
+}
