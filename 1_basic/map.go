5455e1074738e929523c0d2f824638fd4050a3a0
ridaadila <ridaadila10@gmail.com>
2023-05-13 20:40:20 +0700

    add map

---
+++ 1_basic/map.go
@@ -0,0 +1,29 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	person := map[string]string{
+		"nama":   "rida",
+		"alamat": "surabaya",
+	}
+
+	fmt.Println(person)
+	fmt.Println(person["nama"])
+	fmt.Println(person["alamat"])
+
+	person["umur"] = "22"
+
+	fmt.Println(person["umur"])
+
+	buku := make(map[string]string)
+	buku["judul"] = "buku bahasa inggris"
+	buku["pengarang"] = "andi"
+
+	fmt.Println(buku)
+
+	delete(buku, "judul")
+
+	fmt.Println(buku)
+}
