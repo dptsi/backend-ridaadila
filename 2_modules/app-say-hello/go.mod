725a4f237cc1328655192f534379e315f47b7a48
ridaadila <ridaadila10@gmail.com>
2023-06-30 13:59:37 +0700

    add go modules

---
+++ 2_modules/app-say-hello/go.mod
@@ -0,0 +1,5 @@
+module app-say-hello
+
+go 1.20
+
+require github.com/ridaadila/go-rida v1.0.1
---
+++ 2_modules/app-say-hello/main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+
+	go_rida "github.com/ridaadila/go-rida"
+)
+
+func main() {
+
+	fmt.Println(go_rida.SayHello())
+}
